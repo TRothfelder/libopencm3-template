940255a31fe65edbd31e683965af2e91c663bf4b
Tobias Rothfelder <TRothfelder@users.noreply.github.com>
2018-09-04 21:08:02 +0200

    fixed some typos in cmakes, added ugfx to int

--- src/config/clock.c
+++ src/config/clock.c
@@ -4,22 +4,22 @@
  *  Created on: 27.05.2018
  *      Author: kt
  */
-#include <libopencm3/stm32/rcc.h>
-#include <libopencm3/cm3/systick.h>
+ #include <libopencm3/stm32/rcc.h>
+ #include <libopencm3/cm3/systick.h>
 
-#include "config/clock.h"
+ #include "config/clock.h"
 
 
-void setupClock(void) {
-	//setup clock
-	rcc_clock_setup_hse_3v3(&rcc_hse_8mhz_3v3[RCC_CLOCK_3V3_168MHZ]);
-	/* 168MHz / 8 => 21000000 counts per second */
-	systick_set_clocksource(STK_CSR_CLKSOURCE_AHB_DIV8);
-	/* 21000000/21000 = 1000 overflows per second - every 1ms one interrupt */
-	/* SysTick interrupt every N clock pulses: set reload to N-1 */
-	systick_set_reload(20999);
-	systick_interrupt_enable();
-	/* Start counting. */
-	systick_counter_enable();
+ void setupClock(void) {
+ 	//setup clock
+ 	rcc_clock_setup_hse_3v3(&rcc_hse_8mhz_3v3[RCC_CLOCK_3V3_168MHZ]);
+ 	/* 168MHz / 8 => 21000000 counts per second */
+ 	systick_set_clocksource(STK_CSR_CLKSOURCE_AHB_DIV8);
+ 	/* 21000000/21000 = 1000 overflows per second - every 1ms one interrupt */
+ 	/* SysTick interrupt every N clock pulses: set reload to N-1 */
+ 	systick_set_reload(20999);
+ 	systick_interrupt_enable();
+ 	/* Start counting. */
+ 	systick_counter_enable();
 
-}
+ }
--- src/tasks/init.c
+++ src/tasks/init.c
@@ -4,18 +4,38 @@
 #include "tasks/usb.h"
 #include "tasks/ledblink.h"
 
+#include "gfx.h"
+
 #include <libopencm3/stm32/gpio.h>
 
 xTaskHandle initTaskHandle = NULL;
 
 void init_task(void *arg) {
 	(void)arg;
+	//unsigned istr;
+	(void)arg;
+	coord_t		width, height;
+	coord_t		i, j;
+
+	gfxInit();
+	// Code Here
+	// Get the screen size
+
+	gdispSetOrientation(GDISP_ROTATE_270);
+	width = gdispGetWidth();
+	height = gdispGetHeight();
+
+	gdispDrawBox(10, 10, width/2, height/2, Yellow);
+	gdispFillArea(width/2, height/2, width/2-10, height/2-10, Blue);
+	gdispDrawLine(5, 30, width-50, height-40, Red);
+	for(i = 5, j = 0; i < width && j < height; i += 7, j += i/20)
+			gdispDrawPixel(i, j, White);
+
 
 	xTaskCreate(usb_task,"USB",100,NULL,configMAX_PRIORITIES-1, &usbTaskHandle);
 	xTaskCreate(ledblink_task,"LEDBLINK",100,NULL,configMAX_PRIORITIES-1, &ledblinkTaskHandle);
 
 	vTaskDelay(10000 / portTICK_RATE_MS);
-	gpio_toggle(GPIOD,GPIO14);
 	vTaskDelete( initTaskHandle );
 	initTaskHandle = NULL;
 }
